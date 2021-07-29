1f0f393092a00d099abe9782d8004bc039b6f951
Sebastian Olsson <sebelino7@gmail.com>
2021-07-29 14:18:23 +0200

    Add terraform config for GKE cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,33 @@ provider "google" {
   zone    = "europe-west1-c"
 }
 
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name                     = "my-gke-cluster"
+  location                 = "europe-west1"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "europe-west1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+  node_config {
+    preemptible     = true
+    machine_type    = "e2-micro"
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
