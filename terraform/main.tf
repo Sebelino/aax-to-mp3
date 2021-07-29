d5baa2c2050bd7f5d70a086ebb9b80b237565ca7
Sebastian Olsson <sebelino7@gmail.com>
2021-07-29 14:28:57 +0200

    Remove terraform config for GCE instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,35 +39,3 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     ]
   }
 }
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["terraform-vm-instance"]
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
-}
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
-}
-
-resource "google_compute_firewall" "ssh_rule" {
-  name    = "terraform-ssh"
-  network = google_compute_network.vpc_network.name
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  target_tags   = ["terraform-vm-instance"]
-  source_ranges = ["0.0.0.0/0"]
-}
