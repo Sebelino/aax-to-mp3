3325a24dbdd6e8dff9292999e02686c5f122f973
Sebastian Olsson <sebelino7@gmail.com>
2021-07-29 11:51:26 +0200

    Add terraform resources for setting up a GCP VM

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.77.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "modified-voyage-320812"
+  region  = "europe-west1"
+  zone    = "europe-west1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
